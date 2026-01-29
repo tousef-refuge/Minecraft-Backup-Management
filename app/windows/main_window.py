9aec4bb7162ade0a5b9db769faeea3ddc38253e8
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-01-29 16:53:37 +0300

    Added title and logo to main window

--- app/windows/main_window.py
+++ app/windows/main_window.py
@@ -1,4 +1,4 @@
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -6,10 +6,31 @@ class MainWindow(QtWidgets.QWidget):
         self.setWindowTitle("Minecraft Backup Management")
         self.settings = QtCore.QSettings()
 
-        self.main_layout = QtWidgets.QGridLayout(self)
+        self.main_layout = QtWidgets.QHBoxLayout(self)
         self.setLayout(self.main_layout)
         self.main_layout.setContentsMargins(20, 20, 20, 20)
-        self.main_layout.setVerticalSpacing(10)
+        self.main_layout.setSpacing(10)
+
+        self._build_title()
 
         self.adjustSize()
         self.setFixedSize(self.size())
+
+    def _build_title(self):
+        layout = QtWidgets.QHBoxLayout()
+        layout.setSpacing(10)
+
+        logo_pixmap = QtGui.QPixmap("textures/logo.svg").scaled(
+            75, 75,
+            QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+            QtCore.Qt.TransformationMode.SmoothTransformation
+        )
+        logo = QtWidgets.QLabel()
+        logo.setPixmap(logo_pixmap)
+        layout.addWidget(logo)
+
+        title = QtWidgets.QLabel("Backup Management")
+        title.setFont(QtGui.QFont("Courier", 25))
+        layout.addWidget(title)
+
+        self.main_layout.addLayout(layout)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 from app import Application
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets, QtCore, QtGui
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     QtCore.QCoreApplication.setOrganizationName("@tousef_refuge")
     QtCore.QCoreApplication.setOrganizationDomain("MinecraftBackupManagement")
+    app.setWindowIcon(QtGui.QIcon("textures/logo.svg"))
 
     application = Application()
     application.start()
